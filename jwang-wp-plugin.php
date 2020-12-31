87b83801582b3c197ca9ec582ad3849e71ea3b14
Johnny Wang <johnny.wang42@gmail.com>
2020-12-31 18:52:13 -0500

    Basic plugin layout

--- jwang-wp-plugin.php
+++ jwang-wp-plugin.php
@@ -8,3 +8,54 @@ Author: Johnny Wang
 Author URI: https://johnnywang.dev/
 License: GPLv3
 */
+
+register_activation_hook(__FILE__, 'jw_demo_setup');
+/**
+ * Setup permalink, scripts, and styles for the plugin
+ *
+ * @return void
+ */
+function jw_demo_setup()
+{
+    jw_demo_users();
+    flush_rewrite_rules();
+}
+
+register_deactivation_hook(__FILE__, 'jw_demo_cleanup');
+/**
+ * Remove the permalink for the plugin
+ */
+function jw_demo_cleanup()
+{
+    remove_rewrite_tag('jw-demo');
+    remove_permastruct('jw-demo');
+    flush_rewrite_rules();
+}
+
+add_action('init', 'jw_demo_users');
+/**
+ * Hook the permalink for the plugin
+ */
+function jw_demo_users()
+{
+    add_rewrite_tag('%jw-demo%', '([^/]+)');
+    add_permastruct('jw-demo', '/%jw-demo%/');
+}
+
+add_filter('template_include', 'jw_get_users');
+/**
+ * Load template file for displaying the users table
+ * @param string
+ * @return string
+ */
+function jw_get_users($template)
+{
+    $plugin_path = plugin_dir_url(__DIR__);
+    if ($query_var = get_query_var('jw-demo')) {
+        wp_enqueue_script('jw-demo-script', $plugin_path.'/js/jw-demo-script.js', array(), '0.1');
+        wp_enqueue_style('jw-demo-style', $plugin_path.'/style.css', array(), '0.1');
+        return plugin_dir_path(__FILE__).'/templates/users.php';
+    } else {
+        return $template;
+    }
+}
---
+++ templates/users.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template for displaying table of users retrieved from API
+ */
+get_header();
+?>
+    <div class="continer">
+        <h1>Demo Plugin</h1>
+    </div>
+<?php
+get_footer();
+?>
