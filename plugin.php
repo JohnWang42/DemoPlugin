2addfa718e3e7f25eab39411f35bb713602fedd8
Johnny Wang <johnny.wang42@gmail.com>
2021-01-03 12:12:42 -0500

    autoload support

--- plugin.php
+++ plugin.php
@@ -8,6 +8,9 @@ Author: Johnny Wang
 Author URI: https://johnnywang.dev/
 License: GPLv3
 */
+if (is_readable(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+}
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_URL', plugins_url('', __FILE__));
 include PLUGIN_PATH.'src/DemoPlugin.php';
